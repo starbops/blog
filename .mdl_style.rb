37bf545d93211f5f6e3d9b462e8def3f78d966e0
Zespre Schmidt <starbops@zespre.com>
2024-08-11 20:09:28 +0800

    ci: integrate docker for linting and building using make

---
+++ .mdl_style.rb
@@ -0,0 +1,25 @@
+all
+
+# MD002 - First header should be a top level header
+rule 'MD002', :level => 2
+# MD004 - Unordered list style
+rule 'MD004', :style => :dash
+# MD007 - Unordered list indentation
+rule 'MD007', :indent => 3
+# MD009 - Trailing spaces
+rule 'MD009', :br_spaces => 2
+# MD013 - Line length
+rule 'MD013', :ignore_code_blocks => true, :tables => false
+# MD024 - Multiple headers with the same content
+rule 'MD024', :allow_different_nesting => true
+# MD029 - Ordered list item prefix
+rule 'MD029', :style => :one
+# MD030 - Spaces after list markers
+rule 'MD030', :ul_single => 2, :ol_single => 1, :ul_multi => 2, :ol_multi => 1
+
+# MD025 - Multiple top level headers in the same document
+exclude_rule 'MD025'
+# MD028 - Blank line inside blockquote
+exclude_rule 'MD028'
+# MD041 - First line in file should be a top level header
+exclude_rule 'MD041'
